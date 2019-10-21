b630a7bfcb671777379a89aee7ff6763e4994503
BaekGeunYoung <dvmflstm@naver.com>
2019-10-21 18:07:50 +0900

    docker 환경에서 local prisma 및 mysql container를 구동하고, 이 local database와 api server가 연동할 수 있도록 함

--- src/generated/prisma-client/index.js
+++ src/generated/prisma-client/index.js
@@ -16,6 +16,6 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `https://eu1.prisma.sh/dvmflstm-630cbb/graphql-prisma-practice/dev`
+  endpoint: `http://localhost:4466`
 });
 exports.prisma = new exports.Prisma();
